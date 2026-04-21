d1d8d0bfa3e7e29485f7d8dded7932d0a69a3369
Mustapha <mestaphamansouri@gmail.com>
2026-04-21 13:46:41 +0100

    Add resize to transformation

--- src/datamodule.py
+++ src/datamodule.py
@@ -36,12 +36,14 @@ class XrayDataModule(L.LightningDataModule):
         super().__init__()
         self.cfg      = cfg
         self.train_tf = transforms.Compose([
+            transforms.resize((224, 224)),
             transforms.RandomAffine(degrees=10, translate=(0.05, 0.05)),
             transforms.RandomAutocontrast(p=0.3),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ])
         self.val_tf   = transforms.Compose([
+            transforms.resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ])
